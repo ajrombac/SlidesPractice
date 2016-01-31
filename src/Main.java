008b03c82ebe7eca310a160a7cfaa656a925f631
Alex Rombach <ajrombac@oakland.edu>
2016-01-31 16:32:03 -0500

    Added code to do binary to decimal conversion

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to the Binary to Decimal calculator Program\n");
+        System.out.print("Enter binary number value: ");
+        Scanner scanner = new Scanner(System.in);
+        String buffer = scanner.nextLine();
+        System.out.println("You entered: " + buffer + " for conversion.\n");
+        int decimal =  Binary2decimal(buffer);
+        System.out.println("Decimal value of binary " + buffer + " is " + decimal);
     }
+
+    private static int Binary2decimal(String binary) {
+        int result = 0; // Temporarily holds result
+        for(int i = 0; i < binary.length(); i++)
+        {
+            int digit = Character.getNumericValue(binary.charAt(i));
+            int position = binary.length() - i -1;
+            result += digit * Math.pow(2, position);
+        }
+
+        return result;
+    }
+
 }
